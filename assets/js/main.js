84dd5bc0c47937d0ce46b85aecba23ba45332668
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-23 15:33:49 +0100

    Add the bottons in JS, Add the structure for the Bottons in Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
 // ``
-console.log("its working");
-
 const slides = [
     '01.webp',
     '02.webp',
@@ -13,7 +11,13 @@ const activeImage = 0;
 
 const slidesElement = document.querySelector('.slides');
 
+const topElement =document.querySelector('.top');
+
+const bottomElement =document.querySelector('.bottom');
+
 console.log(slides, slidesElement, activeImage);
+
+// Cicle for 
 
 for (let i = 0; i < slides.length; i++) {
     const slide = slides[i];
@@ -21,4 +25,18 @@ for (let i = 0; i < slides.length; i++) {
 
     const slideMarkup = `<img class="${ i === activeImage ? 'active' : '' }" src="./assets/img/${slide}" alt="">`;
     console.log(slideMarkup);
+
+    slidesElement.insertAdjacentHTML('beforeend', slideMarkup)
 }
+
+console.log(topElement, bottomElement);
+
+// Listener Event for the top click
+topElement.addEventListener('click', function(){
+    console.log("click for the previus image");
+})
+
+// Listener Event for the bottom click
+bottomElement.addEventListener('click', function(){
+    console.log("click for the next image");
+})
