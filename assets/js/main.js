18cafe147fe43fd3204bd0c81a06f95211ae0799
emamoldo <moldovanemanuel92.em@gmail.com>
2024-02-23 15:24:23 +0100

    Add Cicle For

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,16 @@ const slides = [
     '05.webp',
 ];
 
+const activeImage = 0;
+
+const slidesElement = document.querySelector('.slides');
+
+console.log(slides, slidesElement, activeImage);
+
+for (let i = 0; i < slides.length; i++) {
+    const slide = slides[i];
+    console.log(slide);
+
+    const slideMarkup = `<img class="${ i === activeImage ? 'active' : '' }" src="./assets/img/${slide}" alt="">`;
+    console.log(slideMarkup);
+}
